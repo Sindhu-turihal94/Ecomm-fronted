9f007ce5627b7dd1f1f6731d2605970aa26d2f96
Sindhu-turihal94 <sindhuturihal94@gmail.com>
2026-01-06 15:26:00 +0530

    changed commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About Page</div>
+    <div>
+      <h1>About Us</h1>
+      <br />
+      <h2>
+        Welcome to ShopEase, your trusted online destination for quality products at affordable prices.
+      </h2>
+      <h2>At ShopEase, we believe shopping should be simple, secure, and enjoyable. Our platform brings together a wide range of products—from fashion and 
+        electronics to home essentials—all carefully selected to meet your everyday needs.</h2>
+        <b>Our Mission</b>
+        <h2>Our mission is to make online shopping accessible to everyone by offering:</h2>
+        <ol>
+          <li>High-quality products</li>
+          <li>Competitive pricing</li>
+          <li>Fast and reliable delivery</li>
+          <li>Excellent customer support</li>
+        </ol>
+    </div>
   )
 }
 
